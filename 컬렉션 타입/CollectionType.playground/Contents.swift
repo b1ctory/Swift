0759a06d020e7923ea829418008931417a2a04c5
천수빈 <suebin710@gmail.com>
2021-09-07 21:21:17 +0900

    add list code

---
+++ 컬렉션 타입/CollectionType.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var numbers: Array<Int> = Array<Int>()
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+numbers.insert(4, at: 2)
+
+numbers.remove(at: 0)
+numbers
+
+var names = [String]()
+var names2: [String] = []
