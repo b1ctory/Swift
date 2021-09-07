97bd9a49808a2846b12a792860c198634d0cfd17
천수빈 <suebin710@gmail.com>
2021-09-07 21:23:43 +0900

    add dictionary code

--- 컬렉션 타입/CollectionType.playground/Contents.swift
+++ 컬렉션 타입/CollectionType.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// List
 var numbers: Array<Int> = Array<Int>()
 numbers.append(1)
 numbers.append(2)
@@ -12,3 +13,16 @@ numbers
 
 var names = [String]()
 var names2: [String] = []
+
+// Dictionary
+var dic: Dictionary<String, Int> = Dictionary<String, Int>()
+var dic2: [String: Int] = ["수빈": 1]
+dic2["수빈"] = 4
+dic2["마마"] = 6
+
+dic2
+dic2["수빈"] = 10
+dic2
+
+dic2.removeValue(forKey: "마마")
+dic2
