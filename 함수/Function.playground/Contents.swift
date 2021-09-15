ffdaadb3db02d20bb09b3789f0354385aa55dca6
천수빈 <suebin710@gmail.com>
2021-09-15 21:05:39 +0900

    add 가변매개변수

--- 함수/Function.playground/Contents.swift
+++ 함수/Function.playground/Contents.swift
@@ -21,3 +21,23 @@ func greeting(friend: String, me: String = "gunter") {
 }
 
 greeting(friend: "Albert")
+
+func sendMessage(from myName: String, to name: String) -> String {
+    return "Hello \(name)! I'm \(myName)"
+}
+
+sendMessage(from: "Gunter", to: "Json")
+
+func sendMessage2(_ myName: String, _ name: String) -> String {
+    return "Hello \(name)! I'm \(myName)"
+}
+
+sendMessage2("hi", "hi")
+
+
+// ... 은 가변매개변수
+func sendMessage3(me : String, friends: String...) -> String {
+    return "Hello \(friends)! I'm \(me)"
+}
+
+sendMessage3(me: "Gunter", friends: "a,b,c,d")
