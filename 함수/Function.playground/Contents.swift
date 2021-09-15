561ca97876520607147597886e86350f67262401
천수빈 <suebin710@gmail.com>
2021-09-15 21:03:07 +0900

    add string print method

--- 함수/Function.playground/Contents.swift
+++ 함수/Function.playground/Contents.swift
@@ -1,8 +1,23 @@
 import UIKit
 
-func sum(a: Int, b: Int) {
+func sum(a: Int, b: Int) -> Int {
     return a+b
 }
 
 sum(a: 5, b: 3)
 
+func hello() -> String {
+    return "hello"
+}
+
+hello()
+
+func printName() {
+    
+}
+
+func greeting(friend: String, me: String = "gunter") {
+    print("Hello, \(friend)! I'm \(me)")
+}
+
+greeting(friend: "Albert")
