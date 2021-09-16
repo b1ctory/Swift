73dde8283d185728a712627dd8bf1792562e3a9b
천수빈 <suebin710@gmail.com>
2021-09-16 23:19:22 +0900

    add switch

--- 조건문/IF_SWITCH.playground/Contents.swift
+++ 조건문/IF_SWITCH.playground/Contents.swift
@@ -46,3 +46,43 @@ if animal == "dog" {
     print("해당하는 동물 사료가 없음")
 }
 
+
+/*
+ switch 비교대상 {
+ case 패턴1:
+     패턴 일치할 때
+ case 패턴2, 패턴3:
+     패턴 2,3이 일치할 때
+ default:
+     어느 패턴과도 일치하지 않을 때
+ }
+ */
+
+let color = "green"
+
+switch color {
+case "red":
+    print("red")
+case "green":
+    print("green")
+case "yellow":
+    print("yellow")
+default:
+    print("no color")
+}
+
+
+let temperature = 30
+
+switch temperature {
+case -20...9:
+    print("겨울입니다.")
+case 10...14:
+    print("가을입니다.")
+case 15...25:
+    print("봄입니다.")
+case 26...35:
+    print("여름 입니다.")
+default:
+    print("이상 기후입니다.")
+}
