58fd7acbc4c94e8ddf301bbd1b1347218ea22bed
천수빈 <suebin710@gmail.com>
2021-09-20 15:18:52 +0900

    add calculate property

--- 프로퍼티/Property.playground/Contents.swift
+++ 프로퍼티/Property.playground/Contents.swift
@@ -27,3 +27,24 @@ class Cat {
 let cat = Cat(name: "json", gender: "male")
 cat.name = "gunter"
 print(cat.name)
+
+
+struct Stock {
+    var averagePrice: Int
+    var quantity: Int
+    var purchasePrice: Int {
+        get {
+            return averagePrice * quantity
+        }
+        
+        set(newPrice) {
+            averagePrice = newPrice / quantity
+        }
+    }
+}
+
+var stock = Stock(averagePrice: 2300, quantity: 3)
+print(stock)
+stock.purchasePrice
+stock.purchasePrice = 3000
+stock.averagePrice
