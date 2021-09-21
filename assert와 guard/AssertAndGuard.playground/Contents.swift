23e6697865ee28d12647ef9d6535a2f6dce13a7c
천수빈 <suebin710@gmail.com>
2021-09-21 12:01:15 +0900

    add guard code

--- assert와 guard/AssertAndGuard.playground/Contents.swift
+++ assert와 guard/AssertAndGuard.playground/Contents.swift
@@ -7,4 +7,20 @@ assert(value == 0)
 
 value = 2
 // 특정 조건체크하고 아니면 에러발생시키면서 메세지 출력
-assert(value == 0, "값이 0이 아닙니다.")
+//assert(value == 0, "값이 0이 아닙니다.")
+
+/*
+ guard 조건 else {
+    // 조건이 false 면 else 구문이 실행되고
+    return or throw or break 를 통해 이후 코드를 실행하지 않도록 한다.
+ */
+
+func guardTest(value: Int?) {
+    guard let value = value else {
+        return
+    }
+    print(value)
+}
+
+guardTest(value: 4)
+guardTest(value: nil)
